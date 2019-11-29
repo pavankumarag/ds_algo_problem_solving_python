18c852601fd861362085099ced5100d7ad9c668b
Pavan Govindraj <pavan.govindraj@nutanix.com>
2019-11-29 20:34:47 +0530

    day 3 problem coding

--- practice/easy/03_closest_value_bst_list.py
+++ practice/easy/03_closest_value_bst_list.py
@@ -1,4 +1,5 @@
-def closest_value_in_list(a, k):
+def closest_value_in_list_logn_my_try(a, k):
+	# assuming a is sorted
 	if len(a) == 0:
 		return None
 	elif len(a) == 1:
@@ -51,9 +52,121 @@ def closest_value_in_list(a, k):
 		return closest_val.keys()
 
 
+def closest_val_in_lst_logn(a, k):
+	if len(a) == 0:
+		return None
+	if len(a) == 1:
+		return a[1]
+	low = 0
+	high = len(a) - 1
+	min_diff = float("inf")
+	right_mid_diff = None
+	left_mid_diff = None
+	closest_val = None
+	mid_diff = None
+	while low <= high:
+		# print a[low:high+1]
+		mid = (low + high) / 2
+		if mid+1 < len(a):
+			right_mid_diff = abs(k - a[mid+1])
+		if mid > 0:
+			left_mid_diff = abs(k - a[mid-1])
+		mid_diff = abs(k - a[mid])
+		if mid_diff == right_mid_diff and mid+1 < len(a):
+				return [a[mid], a[mid+1]]
+		elif mid_diff == left_mid_diff and mid < 0:
+			return [a[mid-1], a[mid]]
+		if right_mid_diff < min_diff:
+			min_diff = right_mid_diff
+			closest_val = a[mid + 1]
+		if left_mid_diff < min_diff:
+			min_diff = left_mid_diff
+			closest_val = a[mid - 1]
+		if k > a[mid]:
+			low = mid + 1
+		elif k < a [mid]:
+			high = mid - 1
+		elif k == a[mid]:
+			return a[mid]
+		print
+	return closest_val
+
+
+class BSTNode:
+	def __init__(self, data):
+		self.data = data
+		self.right = None
+		self.left = None
+
+
+class Solutions:
+	def recursive_closest_val(self, root, val):
+		return self.binary_search(root, val, root.data)
+
+	@staticmethod
+	def closest_val(root, val):
+		closest = root.data
+		while root:
+			if abs(root.data - val) < abs(val - closest): closest = root.data
+			if val < root.data : root = root.left
+			else: root = root.right
+		return closest
+
+	def binary_search(self, root, val, closest):
+		if not root:
+			return closest
+		if abs(root.data - val) < abs(val - closest):
+			closest = root.data
+		if val < root.data: return self.binary_search(root.left, val, closest)
+		elif val > root.data: return self.binary_search(root.right, val, closest)
+		return closest
+
+	def find_closest_val(self, root, k):
+		min_diff = float('inf')
+		min_diff_key = [-1]
+		self.find_closest_helper(root, k, min_diff, min_diff_key)
+		return min_diff_key[0]
+
+	def find_closest_helper(self, root, k, min_diff, min_diff_key):
+		if root is None: return
+		if root.data == k:
+			min_diff_key[0] = root.data
+			return
+		if min_diff > (root.data - k):
+			min_diff = root.data - k
+			min_diff_key[0] = root.data
+		if k < root.data:
+			self.find_closest_helper(root.left, k, min_diff, min_diff_key)
+		else:
+			self.find_closest_helper(root.right, k, min_diff, min_diff_key)
+
+
 if __name__ == "__main__":
-	a = [2,4,9,11,12,19,23,30]
-	k = -1
-	print closest_value_in_list(a, k)
+	a = [2,4,7,11,17,19,23,30]
+	k = 19
+	# print closest_value_in_list_logn_my_try(a, k)
+	import timeit
+	print timeit.timeit("closest_value_in_list_logn_my_try([2,4,5,11,17,19,23,30],12)",
+								setup="from __main__ import closest_value_in_list_logn_my_try",
+								number=1)
+	print timeit.timeit("closest_val_in_lst_logn([2,4,5,11,17,19,23,30],12)",
+								setup="from __main__ import closest_val_in_lst_logn",
+								number=1)
+	print closest_val_in_lst_logn(a, k)
+	root = BSTNode(9)
+	root.left = BSTNode(4)
+	root.left.left = BSTNode(3)
+	root.right = BSTNode(17)
+	root.right.right = BSTNode(22)
+	root.right.right.left = BSTNode(20)
+	root.left.right = BSTNode(6)
+	root.left.right.left = BSTNode(5)
+	root.left.right.right = BSTNode(7)
+	s = Solutions()
+	print s.recursive_closest_val(root, 6)
+	print Solutions.closest_val(root, 6)
+	print s.find_closest_val(root, 6)
+
+
 
 
