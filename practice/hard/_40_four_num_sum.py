454fe156f5a1bffa22929a130ce1fa395f1b0885
Pavan Govindraj <pavan.govindraj@nutanix.com>
2020-01-05 21:52:16 +0530

    day 40 problem solved

---
+++ practice/hard/_40_four_num_sum.py
@@ -0,0 +1,27 @@
+#-* - coding:UTF - 8 -*-
+"""
+Find four elements that sum to a given value
+
+Given an array of integers, find all combination of four elements in the array whose sum is equal to a given value X.
+For example, if the given array is {10, 2, 3, 4, 5, 9, 7, 8} and X = 23,
+then your function should print “3 5 7 8” (3 + 5 + 7 + 8 = 23).
+"""
+
+
+from collections import defaultdict
+def find_four(a, n, k):
+	d = defaultdict(list)
+	for i in range(n):
+		for j in range(i+1, n):
+			sum = a[i] + a[j]
+			if (k - sum) in d.keys():
+				num = d[k - sum]
+				print "four sum found", a[i], a[j], a[num[0]], a[num[1]]
+		for k in range(i):
+			d[a[i]+a[k]] = [i, k]
+
+
+if __name__ == "__main__":
+	a = [1,2,3,4,12,43,32,53,8,-10,4]
+	find_four(a, len(a),17)
+	find_four(a, len(a), 55)
