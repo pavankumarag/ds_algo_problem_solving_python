39b24133e4b2a25028114ec19193c8eba4b5970d
Pavan Govindraj <pavan.govindraj@nutanix.com>
2020-01-08 15:14:27 +0530

    added n2 time method

--- practice/hard/_40_four_num_sum.py
+++ practice/hard/_40_four_num_sum.py
@@ -11,7 +11,16 @@ then your function should print “3 5 7 8” (3 + 5 + 7 + 8 = 23).
 from collections import defaultdict
 
 
-def find_four(a, n, k): # Takes O(n2), best possible
+def find_four_n4(a, n, sum):
+	for i in range(n):
+		for j in range(i+1, n):
+			for k in range(j+1, n):
+				for l in range(k+1, n):
+					if a[i] + a[j] + a[k] + a[l] == sum:
+						print a[i], a[j], a[k], a[l]
+
+
+def find_four_n2(a, n, k): # Takes O(n2), best possible
 	d = defaultdict(list)
 	for i in range(n):
 		for j in range(i+1, n):
@@ -25,5 +34,6 @@ def find_four(a, n, k): # Takes O(n2), best possible
 
 if __name__ == "__main__":
 	a = [1,2,3,4,12,43,32,53,8,-10,4]
-	#find_four(a, len(a),17)
-	find_four(a, len(a), 59)
+	find_four_n2(a, len(a),17)
+	#find_four_n2(a, len(a), 59)
+	find_four_n4(a, len(a),17)
