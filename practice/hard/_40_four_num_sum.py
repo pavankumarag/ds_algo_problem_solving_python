ef16720f70e5efe0343c7dea0751691e611ba729
Pavan Govindraj <pavan.govindraj@nutanix.com>
2020-01-08 10:27:51 +0530

    variable messup

--- practice/hard/_40_four_num_sum.py
+++ practice/hard/_40_four_num_sum.py
@@ -9,7 +9,9 @@ then your function should print “3 5 7 8” (3 + 5 + 7 + 8 = 23).
 
 
 from collections import defaultdict
-def find_four(a, n, k):
+
+
+def find_four(a, n, k): # Takes O(n2), best possible
 	d = defaultdict(list)
 	for i in range(n):
 		for j in range(i+1, n):
@@ -17,11 +19,11 @@ def find_four(a, n, k):
 			if (k - sum) in d.keys():
 				num = d[k - sum]
 				print "four sum found", a[i], a[j], a[num[0]], a[num[1]]
-		for k in range(i):
-			d[a[i]+a[k]] = [i, k]
+		for l in range(i):
+			d[a[i]+a[l]] = [i, l]
 
 
 if __name__ == "__main__":
 	a = [1,2,3,4,12,43,32,53,8,-10,4]
-	find_four(a, len(a),17)
-	find_four(a, len(a), 55)
+	#find_four(a, len(a),17)
+	find_four(a, len(a), 59)
