8c787402e9db435b3f469c7d03c1d5c8614e0966
pavankumarag <pavankumar.ag@gmail.com>
2020-09-14 19:29:29 +0530

    adding cache decorator implementation of fib

--- practice/easy/_01_fibonacci_all_variants.py
+++ practice/easy/_01_fibonacci_all_variants.py
@@ -28,6 +28,21 @@ def fib_formula(n):
 	import math
 	return math.pow((math.sqrt(5) + 1)/2, n)/math.sqrt(5)
 
+def cache(foo):
+    cache_map = {}
+    def wrapped(*args):
+        if args not in cache_map:
+            print args,foo(*args)
+            cache_map[args] = foo(*args)
+        else:
+            print('Using from cache')
+        return cache_map[args]
+    return wrapped
+
+@cache
+def fib(n):
+    return n if n == 0 or n == 1 else fib(n-1) + fib(n-2)
+
 if __name__ == "__main__":
 	for i in range(0,51):
 		f.append(-1)
